171460575c1b3c408c68710eebb8ac86cfd5796e
Wunder, Evan <ejwunde@ilstu.edu>
2018-04-13 16:44:28 -0500

    upadte package-lock.json, package.json, and home.js page

--- src/app/components/home/home.js
+++ src/app/components/home/home.js
@@ -3,36 +3,38 @@ import Fold from "pawjs/src/components/fold";
 import ReactLogo from "src/resources/images/reactjs.svg";
 import SmallImage from "src/resources/images/mario-large.png?sizes=100w+200w+400w+800w&placeholder";
 import Picture from "pawjs/src/components/picture/picture";
+// import axios from "axios";
 
 export default class Home extends Component {
+  // initialize state
+  state = {
+    selectedFile: null
+  }
+
+  fileSelectedHandler = event => {
+    console.log("file chosen:");
+    // select first file in array
+    console.log(event.target.files[0]);
+    this.setState({
+      selectedFile: event.target.files[0]
+    });
+  }
+
+  fileSelectedHandler = () => {
+    console.log("POST");  
+  }
+
   render() {
     return (
       <div className="mt-4">
         <img style={{ maxWidth: "150px" }} className="img-fluid mx-auto d-block" src={ReactLogo} alt="ReactJS"/>
         <h1 className="h2 text-center mt-3">React PWA</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Mauris bibendum enim quis ligula congue consequat. Maecenas
-          a neque ac diam elementum sollicitudin. Praesent a libero sit
-          amet justo viverra mattis vitae ac nulla. Aenean eget tincidunt
-          urna. Suspendisse quis iaculis ex. Proin nec ante eros. Donec
-          eu eros vitae nunc auctor imperdiet. Praesent nec tortor eget
-          magna gravida posuere. Sed at velit at est dictum mollis. Pellentesque
-          ullamcorper dapibus nulla, efficitur pharetra nisi sodales ut.
-        </p>
+
+        <input type = "file" onChange={this.fileSelectedHandler}/>
+        <button onClick={this.fileSelectedHandler}>Upload</button>
+       
         <Fold placeholder={() => <div className="text-center mt-3 mb-3">Loading folded content.. Please wait..</div>}>
-          <p>
-            Cras at elementum magna. Proin sed vulputate erat. Quisque magna nibh,
-            posuere in mattis id, consectetur non nunc. Sed sodales diam risus,
-            non facilisis augue cursus id. Nulla non magna at tellus tempor
-            efficitur id nec ex. Vestibulum a ex a dolor dapibus vulputate.
-            Etiam ac diam vestibulum, commodo lacus nec, feugiat diam. Mauris
-            sollicitudin pellentesque pretium. Suspendisse at tempus lectus.
-            Vivamus euismod velit quis tincidunt pulvinar. In posuere, libero sed
-            condimentum fringilla, tortor mi maximus dolor, eget aliquam turpis
-            libero at sem. Ut pulvinar, odio a vestibulum pretium, ante leo volutpat
-            massa, in suscipit diam tortor id odio. Nunc convallis vitae felis eget aliquet.
-          </p>
+          
           <p>Image sample converted to webp, uploaded/coded as jpg</p>
           <Picture
             pictureClassName="d-inline-block my-4"
